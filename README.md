db4a0cba9214d0a33322dfc8b861befd6f742c70
Werawad Runagjaroon <lennon@Werawads-MacBook-Air.local>
2017-03-24 12:43:11 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Spring Boot Spark Elasticsearch
+
+## Installation
+
+### Spark
+
+    <dependency>
+        <groupId>org.apache.spark</groupId>
+        <artifactId>spark-core_2.11</artifactId>
+        <version>2.1.0</version>
+    </dependency>
+
+### Elasticsearch Hadoop
+
+
+    <dependency>
+      <groupId>org.elasticsearch</groupId>
+      <artifactId>elasticsearch-hadoop</artifactId>
+      <version>5.0.0</version>
+    </dependency>
+    
+ ### Hibernate-validator
+  - [spark-maven-dependency]('http://stackoverflow.com/questions/40159372/spark-maven-dependency-breaks-down-sprint-boot-application'')
+ 
+ 
+     <dependency>
+         <groupId>org.hibernate</groupId>
+         <artifactId>hibernate-validator</artifactId>
+         <version>5.3.0.Final</version>
+     </dependency>
---
+++ src/main/java/com/example/lennon/SpringBootSparkToElasticsearchApplication.java
@@ -0,0 +1,101 @@
+package com.example.lennon;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spark_project.guava.collect.ImmutableList;
+import org.spark_project.guava.collect.ImmutableMap;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+
+/**
+ * Created by lennon on 3/23/2017 AD.
+ */
+
+@SpringBootApplication
+@ConfigurationProperties(prefix = "spark.es")
+public class SpringBootSparkToElasticsearchApplication implements ApplicationRunner {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(SpringBootSparkToElasticsearchApplication.class);
+
+    private static final String NODES = "spark.es.nodes";
+    private static final String PORT = "spark.es.port";
+    private static final String RESOURCE = "spark.es.resource";
+    private static final String INDEX_AUTO_CREATE = "spark.es.index.auto.create";
+
+    private String nodes;
+    private String port;
+    private String resource;
+    private String indexAutoCreate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSparkToElasticsearchApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+
+        LOGGER.info("SpringBootSparkToElasticsearchApplication: {}, {}, {}, {}", nodes, port, resource, indexAutoCreate);
+
+        String master = "local[*]";
+
+        SparkConf conf = new SparkConf()
+                .setAppName(SpringBootSparkToElasticsearchApplication.class.getName())
+                .setMaster(master);
+
+        conf.set(NODES, nodes);
+        conf.set(PORT, port);
+        conf.set(RESOURCE, resource);
+        conf.set(INDEX_AUTO_CREATE, indexAutoCreate);
+
+        JavaSparkContext context = new JavaSparkContext(conf);
+
+        Map<String, ?> nameOne = ImmutableMap.of("name", "one");
+        Map<String, ?> nameTwo = ImmutableMap.of("name", "two");
+
+        JavaRDD<Map<String, ?>> javaRDD = context.parallelize(ImmutableList.of(nameOne, nameTwo));
+        JavaEsSpark.saveToEs(javaRDD, resource);
+
+    }
+
+    public String getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(String nodes) {
+        this.nodes = nodes;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public String getIndexAutoCreate() {
+        return indexAutoCreate;
+    }
+
+    public void setIndexAutoCreate(String indexAutoCreate) {
+        this.indexAutoCreate = indexAutoCreate;
+    }
+}
